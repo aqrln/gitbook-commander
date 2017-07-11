dd0ad052804dbb2cffb4c5d823f83454575ca2c8
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-07-11 09:04:03 +0300

    style(scripts): replace tabs with spaces

--- scripts/lint-commit-ci.sh
+++ scripts/lint-commit-ci.sh
@@ -4,11 +4,11 @@ set -e
 set -u
 
 if [[ $TRAVIS_PULL_REQUEST_SLUG != "" && $TRAVIS_PULL_REQUEST_SLUG != $TRAVIS_REPO_SLUG ]]; then
-	git remote add "$TRAVIS_PULL_REQUEST_SLUG" "https://github.com/$TRAVIS_PULL_REQUEST_SLUG.git"
-	git fetch "$TRAVIS_PULL_REQUEST_SLUG"
-	TO="$TRAVIS_PULL_REQUEST_SLUG/$TRAVIS_PULL_REQUEST_BRANCH"
+  git remote add "$TRAVIS_PULL_REQUEST_SLUG" "https://github.com/$TRAVIS_PULL_REQUEST_SLUG.git"
+  git fetch "$TRAVIS_PULL_REQUEST_SLUG"
+  TO="$TRAVIS_PULL_REQUEST_SLUG/$TRAVIS_PULL_REQUEST_BRANCH"
 else
-	TO=$TRAVIS_COMMIT
+  TO=$TRAVIS_COMMIT
 fi
 
 conventional-changelog-lint --from="$TRAVIS_BRANCH" --to="$TO"
